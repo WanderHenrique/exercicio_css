82b73893f1472d13269ece63628d0aed54229a03
WanderHenrique <112206215+WanderHenrique@users.noreply.github.com>
2023-05-01 13:53:59 -0300

    Delete js.js

--- Exercicio CSS/js.js
+++
@@ -1,9 +0,0 @@
-const minhaImagem = document.querySelector('#minha-imagem');
-
-setInterval(() => {
-    if (minhaImagem.style.opacity === '0') {
-        minhaImagem.style.opacity = '1';
-    } else {
-        minhaImagem.style.opacity = '0';
-    }
-}, 2000);
